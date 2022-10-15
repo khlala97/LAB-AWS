8fcdf3cf9357880fba931a00287cb20679fe1574
Youssef Bendiha <bendiha.youssef@gmail.com>
2022-10-15 12:47:14 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,28 @@
+#
+# error Function
+#
+function error {
+  
+}
+
+#
+# prepare env (keypairs,rules..) Functions
+#
+function env {
+
+}
+
+#
+# deployement Function
+#
+
+function dep {
+
+}
+
+#
+# benchmarking Function
+#
+function benchmarking {
+
+}
